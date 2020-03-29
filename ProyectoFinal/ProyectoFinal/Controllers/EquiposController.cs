23d8678278015e458c45bf3202116673f25d11a4
felipe <felipe.orivas@gmail.com>
2020-03-29 19:34:12 -0300

    Proyecto .Net Terminado

---
+++ ProyectoFinal/ProyectoFinal/Controllers/EquiposController.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Globalization;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private FutGolEntities db = new FutGolEntities();
+
+        // GET: Equipos
+        public ActionResult Index()
+        {
+            return View(db.Equipos.ToList());
+        }
+
+        // GET: Equipos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipos equipos = db.Equipos.Find(id);
+            if (equipos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipos);
+        }
+
+        // GET: Equipos/Create
+        public ActionResult Create()
+        {
+            if (TempData["mensajeError"] != null)
+            {
+                ViewBag.mensajeError = TempData["mensajeError"].ToString();
+                if (TempData["tempEquipo"] != null) ViewBag.tempEquipo = TempData["tempEquipo"].ToString();
+                if (TempData["tempPais"] != null) ViewBag.tempPais = TempData["tempPais"].ToString();
+            }
+
+            return View();
+        }
+
+        // POST: Equipos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,pais")] Equipos equipos)
+        {
+            if (ModelState.IsValid)
+            {
+                // *************** Validar que el nombre de equipo haya sido ingresado
+                if (equipos.nombre == null)
+                {
+                    TempData["mensajeError"] = "Ingrese un nombre de Equipo con 3 o mas Caracteres";
+
+                    // Si ademas se ha ingresado un pais, entonces lo devuelvo para no perderlo
+                    if (equipos.pais != null && equipos.pais.Length > 0) TempData["tempPais"] = equipos.pais;
+
+                    return RedirectToAction("Create");
+                }
+
+                // Validar que el nombre de equipo sea mayor a 2 (o no menor que 3)
+                if (equipos.nombre.Length < 3)
+                {
+                   // Si se ha ingresado un nombre de equipo con 1 o 2 caracteres, entonces lo devuelvo para no perderlo
+                   TempData["mensajeError"] = "Ingrese un nombre de Equipo con 3 o mas Caracteres";
+                   TempData["tempEquipo"] = equipos.nombre;
+
+                   // Si ademas se ha ingresado un pais, entonces lo devuelvo para no perderlo
+                   if(equipos.pais != null && equipos.pais.Length > 0) TempData["tempPais"] = equipos.pais;
+                 
+                   return RedirectToAction("Create");
+                }
+
+                // ************** Validar que el nombre de pais haya sido ingresado
+                if (equipos.pais == null)
+                {
+                    TempData["mensajeError"] = "Ingrese un nombre de pais con 1 o mas Caracteres";
+
+                    // En estas lineas, ya se ha asegurado que se ha ingresado un nombre de equipo valido, entonces lo devuelvo para no perderlo
+                    TempData["tempEquipo"] = equipos.nombre;
+
+                    return RedirectToAction("Create");
+                }
+
+                // Le da el mismo formato (Primera letra de cada palabra en mayuscula) a todos los paises ingresados para que el filtro select de paises, no muestre dos veces o mas el mismo pais que se ha escrito, de distinta manera.
+                equipos.pais = new CultureInfo("en-US", false).TextInfo.ToTitleCase(equipos.pais);
+                string nuevoNombre = equipos.nombre.ToLower();
+
+                // ************** Validar que el nombre de equipo no este repetido si ya existe el pais ingresado
+                List<Equipos> equiposRegistrados = db.Equipos.ToList();
+                Boolean existePais = equiposRegistrados.Exists(x => x.pais == equipos.pais && x.nombre.ToLower() == nuevoNombre);
+
+                if(existePais)
+                {
+                    TempData["mensajeError"] = "El nombre de Equipo ingresado ya existe en el pais ingresado, ingrese otro";
+                    TempData["tempEquipo"] = equipos.nombre;
+                    TempData["tempPais"] = equipos.pais;
+
+                    return RedirectToAction("Create");
+                }
+
+                db.Equipos.Add(equipos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(equipos);
+        }
+
+        // GET: Equipos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipos equipos = db.Equipos.Find(id);
+            if (equipos == null)
+            {
+                return HttpNotFound();
+            }
+            if (TempData["mensajeError"] != null)
+            {
+                ViewBag.mensajeError = TempData["mensajeError"].ToString();
+                if (TempData["tempEquipo"] != null) ViewBag.tempEquipo = TempData["tempEquipo"].ToString();
+                if (TempData["tempPais"] != null) ViewBag.tempPais = TempData["tempPais"].ToString();
+            }
+            return View(equipos);
+        }
+
+        // POST: Equipos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,pais")] Equipos equipos)
+        {
+            if (ModelState.IsValid)
+            {
+
+                // *************** Validar que el nombre de equipo haya sido ingresado
+                if (equipos.nombre == null)
+                {
+                    TempData["mensajeError"] = "Ingrese un nombre de Equipo con 3 o mas Caracteres";
+
+                    // Si ademas se ha ingresado un pais, entonces lo devuelvo para no perderlo
+                    if (equipos.pais != null && equipos.pais.Length > 0) TempData["tempPais"] = equipos.pais;
+
+                    return RedirectToAction("Edit");
+                }
+
+                // Validar que el nombre de equipo sea mayor a 2 (o no menor que 3)
+                if (equipos.nombre.Length < 3)
+                {
+                    // Si se ha ingresado un nombre de equipo con 1 o 2 caracteres, entonces lo devuelvo para no perderlo
+                    TempData["mensajeError"] = "Ingrese un nombre de Equipo con 3 o mas Caracteres";
+                    TempData["tempEquipo"] = equipos.nombre;
+
+                    // Si ademas se ha ingresado un pais, entonces lo devuelvo para no perderlo
+                    if (equipos.pais != null && equipos.pais.Length > 0) TempData["tempPais"] = equipos.pais;
+
+                    return RedirectToAction("Edit");
+                }
+
+                // ************** Validar que el nombre de pais haya sido ingresado
+                if (equipos.pais == null)
+                {
+                    TempData["mensajeError"] = "Ingrese un nombre de pais con 1 o mas Caracteres";
+
+                    // En estas lineas, ya se ha asegurado que se ha ingresado un nombre de equipo valido, entonces lo devuelvo para no perderlo
+                    TempData["tempEquipo"] = equipos.nombre;
+
+                    return RedirectToAction("Edit");
+                }
+
+                // ************** Validar que el nombre de equipo no este repetido si ya existe el pais ingresado
+
+                // Primero obtener el nombre original del equipo
+                var equipoOriginal = db.Equipos.Where(x => x.id == equipos.id).Select(col => new { col.nombre, col.pais }).First();
+                string nombreOriginal = equipoOriginal.nombre.ToLower();
+                string paisOriginal = equipoOriginal.pais.ToLower();
+
+                string nuevoNombre = equipos.nombre.ToLower();
+
+                // Le da el mismo formato (Primera letra de cada palabra en mayuscula) a todos los paises ingresados para que el filtro select de paises, no muestre dos veces o mas el mismo pais que se ha escrito, de distinta manera.
+                equipos.pais = new CultureInfo("en-US", false).TextInfo.ToTitleCase(equipos.pais);
+
+                string nuevoPais = equipos.pais.ToLower();
+
+                // Validar si han cambiado los atributos
+                if(nombreOriginal.Equals(nuevoNombre) && paisOriginal.Equals(nuevoPais))
+                {
+                    // No ha habido cambio alguno
+                    db.Entry(equipos).State = EntityState.Unchanged;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                } else
+                {
+                    // ha cambiado el nombre de Equipo o el Pais
+                    
+                    if (db.Equipos.Any(a => a.nombre == nuevoNombre && a.pais == nuevoPais))
+                    {
+                        TempData["mensajeError"] = "El nombre de Equipo ingresado ya existe en el pais ingresado, ingrese otro";
+                        TempData["tempEquipo"] = equipos.nombre;
+                        TempData["tempPais"] = equipos.pais;
+
+                        return RedirectToAction("Edit");
+                    }
+                }
+                db.Entry(equipos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(equipos);
+        }
+
+        // GET: Equipos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipos equipos = db.Equipos.Find(id);
+            if (equipos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipos);
+        }
+
+        // POST: Equipos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Equipos equipos = db.Equipos.Find(id);
+
+            // Elimina todos los jugadores con el id_equipo,el cual se va a eliminar para que la aplicacion no de errores
+            db.Database.ExecuteSqlCommand("DELETE FROM Jugadores WHERE id_equipo = @par1",
+                    new SqlParameter("@par1", equipos.id));
+
+            db.Equipos.Remove(equipos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoFinal/ProyectoFinal/Controllers/JugadoresController.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Controllers
+{
+    public class JugadoresController : Controller
+    {
+        private FutGolEntities db = new FutGolEntities();
+
+        // GET: Jugadores
+        public ActionResult Index()
+        {
+            // Obtener los paises registrados, sin repetirse. Obtener el pais 2 veces para que el nuevo select list tenga como key y value el nombre del pais
+            var paises = db.Equipos.Select(col => new { key = col.pais, value = col.pais} ).Distinct();
+
+            // Devolver el select para filtrar por paises registrados
+            ViewBag.Paises = new SelectList(paises.ToList(), "key", "value");
+            ViewBag.paisSeleccion = null;
+            var jugadores = db.Jugadores.Include(j => j.Equipos);
+            return View(jugadores.ToList());
+        }
+
+        // Post: Jugadores
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string Paises)
+        {
+            string tempPais = Paises.ToString();
+
+            var paises = db.Equipos.Select(col => new { key = col.pais, value = col.pais }).Distinct();
+
+            // Devolver el mismo select con el pais seleccionado
+            ViewBag.Paises = new SelectList(paises.ToList(), "key", "value", Paises);
+
+            // Devolver los jugadores por el pais seleccionado
+            var jugadoresPorPais = db.Jugadores.Include(j => j.Equipos).Where(p => p.Equipos.pais == tempPais);
+            return View(jugadoresPorPais.ToList());
+
+        }
+
+            // GET: Jugadores/Details/5
+            public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jugadores jugadores = db.Jugadores.Find(id);
+            if (jugadores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jugadores);
+        }
+
+        // GET: Jugadores/Create
+        public ActionResult Create()
+        {
+            if (TempData["mensajeError"] != null)
+            {
+                ViewBag.mensajeError = TempData["mensajeError"].ToString();
+                if (TempData["tempNombre"] != null) ViewBag.tempNombre = TempData["tempNombre"].ToString();
+                if (TempData["tempApellidos"] != null) ViewBag.tempApellidos = TempData["tempApellidos"].ToString();
+                if (TempData["tempPosicion"] != null) ViewBag.tempPosicion = TempData["tempPosicion"].ToString();
+            }
+            ViewBag.id_equipo = new SelectList(db.Equipos, "id", "nombre");
+            return View();
+        }
+
+        // POST: Jugadores/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombres,apellidos,posicion,id_equipo")] Jugadores jugadores)
+        {
+
+            if (ModelState.IsValid)
+            {
+                // *************** Validar que el nombre del jugador haya sido ingresado y sea mayor a 2 (o no menor que 3)
+                if (jugadores.nombres == null)
+                {
+                    TempData["mensajeError"] = "Ingrese un nombre con 3 o mas Caracteres";
+
+                    // Si ademas se ha ingresado apellidos y posicion entonces los devuelvo para no perderlo
+                    if (jugadores.apellidos != null && jugadores.apellidos.Length > 0) TempData["tempApellidos"] = jugadores.apellidos;
+                    if (jugadores.posicion != null && jugadores.posicion.Length > 0) TempData["tempPosicion"] = jugadores.posicion;
+
+                    return RedirectToAction("Create");
+                }
+
+                // Validar que el nombre de jugador sea mayor a 2 (o no menor que 3)
+                if (jugadores.nombres.Length < 3)
+                {
+                    TempData["mensajeError"] = "Ingrese un nombre con 3 o mas Caracteres";
+
+                    // devuelvo el nombre ingresado con menos de 3 letras
+                    TempData["tempNombre"] = jugadores.nombres;
+
+                    // Si ademas se ha ingresado apellidos y posicion entonces los devuelvo para no perderlo
+                    if (jugadores.apellidos != null && jugadores.apellidos.Length > 0) TempData["tempApellidos"] = jugadores.apellidos;
+                    if (jugadores.posicion != null && jugadores.posicion.Length > 0) TempData["tempPosicion"] = jugadores.posicion;
+
+                    return RedirectToAction("Create");
+                }
+
+                // *************** Validar que apellidos del jugador hayan sido ingresado
+                if (jugadores.apellidos == null)
+                {
+                    TempData["mensajeError"] = "El campo apellidos debe contener 3 o mas Caracteres";
+
+                    // En estas lineas ya se ha validado que se ha ingresado un nombre valido, por lo tanto, lo devuelvo
+                    TempData["tempNombre"] = jugadores.nombres;
+                    if (jugadores.posicion != null && jugadores.posicion.Length > 0) TempData["tempPosicion"] = jugadores.posicion;
+
+                    return RedirectToAction("Create");
+                }
+
+                // Validar que los apellidos sean mayor a 2 (o no menor que 3)
+                if (jugadores.apellidos.Length < 3)
+                {
+                    TempData["mensajeError"] = "El campo apellidos debe contener 3 o mas Caracteres";
+
+                    // devuelvo el nombre ingresado con menos de 3 letras
+                    TempData["tempNombre"] = jugadores.nombres;
+                    TempData["tempApellidos"] = jugadores.apellidos;
+
+                    // Si ademas se ha ingresado posicion entonces la devuelvo para no perderla
+                    if (jugadores.posicion != null && jugadores.posicion.Length > 0) TempData["tempPosicion"] = jugadores.posicion;
+
+                    return RedirectToAction("Create");
+                }
+
+                if (jugadores.apellidos == null)
+                {
+                    TempData["mensajeError"] = "El campo apellidos debe contener 3 o mas Caracteres";
+
+                    // Si ademas se ha ingresado nombres y posicion entonces los devuelvo para no perderlo
+                    TempData["tempNombres"] = jugadores.nombres;
+                    if (jugadores.posicion != null && jugadores.posicion.Length > 0) TempData["tempPosicion"] = jugadores.posicion;
+
+                    return RedirectToAction("Create");
+                }
+
+                string nuevaPosicion = "";
+
+                if (jugadores.posicion != null)
+                {
+                    // Para que el programa no crashee si la posicion ingresada tiene largo mas que 10
+                    nuevaPosicion = jugadores.posicion.ToString();
+                    if(nuevaPosicion.Length > 10) nuevaPosicion = nuevaPosicion.Substring(0, 10);
+                } 
+      
+                db.Database.ExecuteSqlCommand("INSERT INTO Jugadores(nombres, apellidos, posicion, id_equipo) VALUES(@par1, @par2, @par3, @par4)",
+                    new SqlParameter("@par1", jugadores.nombres.ToString()),
+                    new SqlParameter("@par2", jugadores.apellidos.ToString()),
+                    new SqlParameter("@par3", nuevaPosicion),
+                    new SqlParameter("@par4", jugadores.id_equipo));
+
+                // Por algun motivo, me da error al guardar el nuevo jugador usando este metodo, por lo tanto use la query anterior
+                //db.Jugadores.Add(jugadores);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_equipo = new SelectList(db.Equipos, "id", "nombre", jugadores.id_equipo);
+            return View(jugadores);
+        }
+
+        // GET: Jugadores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jugadores jugadores = db.Jugadores.Find(id);
+            if (jugadores == null)
+            {
+                return HttpNotFound();
+            }
+            if (TempData["mensajeError"] != null)
+            {
+                ViewBag.mensajeError = TempData["mensajeError"].ToString();
+                if (TempData["tempNombre"] != null) ViewBag.tempNombre = TempData["tempNombre"].ToString();
+                if (TempData["tempApellidos"] != null) ViewBag.tempApellidos = TempData["tempApellidos"].ToString();
+                if (TempData["tempPosicion"] != null) ViewBag.tempPosicion = TempData["tempPosicion"].ToString();
+            }
+            ViewBag.id_equipo = new SelectList(db.Equipos, "id", "nombre", jugadores.id_equipo);
+            return View(jugadores);
+        }
+
+        // POST: Jugadores/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombres,apellidos,posicion,id_equipo")] Jugadores jugadores)
+        {
+            if (ModelState.IsValid)
+            {
+                // *************** Validar que el nombre del jugador haya sido ingresado y sea mayor a 2 (o no menor que 3)
+                if (jugadores.nombres == null)
+                {
+                    TempData["mensajeError"] = "Ingrese un nombre con 3 o mas Caracteres";
+
+                    // Si ademas se ha ingresado apellidos y posicion entonces los devuelvo para no perderlo
+                    if (jugadores.apellidos != null && jugadores.apellidos.Length > 0) TempData["tempApellidos"] = jugadores.apellidos;
+                    if (jugadores.posicion != null && jugadores.posicion.Length > 0) TempData["tempPosicion"] = jugadores.posicion;
+
+                    return RedirectToAction("Edit");
+                }
+
+                // Validar que el nombre de jugador sea mayor a 2 (o no menor que 3)
+                if (jugadores.nombres.Length < 3)
+                {
+                    TempData["mensajeError"] = "Ingrese un nombre con 3 o mas Caracteres";
+
+                    // devuelvo el nombre ingresado con menos de 3 letras
+                    TempData["tempNombre"] = jugadores.nombres;
+
+                    // Si ademas se ha ingresado apellidos y posicion entonces los devuelvo para no perderlo
+                    if (jugadores.apellidos != null && jugadores.apellidos.Length > 0) TempData["tempApellidos"] = jugadores.apellidos;
+                    if (jugadores.posicion != null && jugadores.posicion.Length > 0) TempData["tempPosicion"] = jugadores.posicion;
+
+                    return RedirectToAction("Edit");
+                }
+
+                // *************** Validar que apellidos del jugador hayan sido ingresado
+                if (jugadores.apellidos == null)
+                {
+                    TempData["mensajeError"] = "El campo apellidos debe contener 3 o mas Caracteres";
+
+                    // Si ademas se ha ingresado nombres y posicion entonces los devuelvo para no perderlo
+                    TempData["tempNombre"] = jugadores.nombres;
+                    if (jugadores.posicion != null && jugadores.posicion.Length > 0) TempData["tempPosicion"] = jugadores.posicion;
+
+                    return RedirectToAction("Edit");
+                }
+
+                // Validar que los apellidos sean mayor a 2 (o no menor que 3)
+                if (jugadores.apellidos.Length < 3)
+                {
+                    TempData["mensajeError"] = "El campo apellidos debe contener 3 o mas Caracteres";
+
+                    // devuelvo nombres y apellidos ingresados con menos de 3 letras
+                    TempData["tempNombre"] = jugadores.nombres;
+                    TempData["tempApellidos"] = jugadores.apellidos;
+
+                    // Si ademas se ha ingresado posicion entonces la devuelvo para no perderla
+                    if (jugadores.posicion != null && jugadores.posicion.Length > 0) TempData["tempPosicion"] = jugadores.posicion;
+
+                    return RedirectToAction("Edit");
+                }
+
+                // Para que no de problemas al guardar cuando el campo posicion ha sido modificado por vacio.
+                if (jugadores.posicion == null) jugadores.posicion = "";
+
+                // Para que el programa no crashee si la posicion ingresada tiene largo mas que 10
+                if (jugadores.posicion != null && jugadores.posicion.Length > 10) jugadores.posicion = jugadores.posicion.Substring(0, 10);
+
+                db.Entry(jugadores).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_equipo = new SelectList(db.Equipos, "id", "nombre", jugadores.id_equipo);
+            return View(jugadores);
+        }
+
+        // GET: Jugadores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jugadores jugadores = db.Jugadores.Find(id);
+            if (jugadores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jugadores);
+        }
+
+        // POST: Jugadores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Jugadores jugadores = db.Jugadores.Find(id);
+            db.Jugadores.Remove(jugadores);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
